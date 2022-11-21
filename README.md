bc8bbc3148afd22312fbef49d0b0614c275aa1d6
Jakob McBroome <jmcbroome@gmail.com>
2022-11-21 13:03:24 -0800

    update documentation

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # automated-lineage-json
-Streamlit webapp for the annotation of Nextstrain Auspice v2 JSON with [genotype representation labels](https://github.com/jmcbroome/automate-lineages-prototype).
+Streamlit webapp for the annotation of Nextstrain Auspice v2 JSON with 
+
+## Summary
+
+This repository contain both a CLI implementation and a Streamlit-based GUI for the application of a lineage nomenclature based on the [genotype representation heuristic](https://github.com/jmcbroome/automate-lineages-prototype).
+
+A public version of the app can be found at: https://jmcbroome-automated-lineage-json-streamlit-app-3adskh.streamlit.app/
+
+## How To Use
+
+The app takes a set of parameters, mostly numeric, and an input [Auspice v2 JSON](https://docs.nextstrain.org/projects/auspice/en/stable/releases/v2.html). The user sets the parameters in the indicated fields and uploads the JSON from local storage, then clicks the run button.
+After completion, an annotated JSON and a simple table of sample-lineage associations become available to download. Once downloaded, the 
+JSON can be drag-and-dropped into the Auspice view directly below the main app, or into a separate tab of auspice.us.
+
+### Explanation of Method and Parameters
+
+This method works by computing the genotype representation index, explained in detail [here](https://github.com/jmcbroome/automate-lineages-prototype#mathematical-underpinnings). Essentially, this metric is high for nodes that represent clusters of samples that are distinct from their parent lineage and relatively uniform within the group. It is computed for every node on the input tree, then the node with the highest value is chosen as a new lineage root. This is repeated many times. The resulting nomenclature is genotype-based and hierarchical, with a simplified Pango-style naming schema. For example, the lineage A.1.1 is a sublineage of A.1, which in turn is a sublineage of group A. Each of the these would be considered a 'level' of annotation. The nomenclature is generated iteratively; each 'level' is generated as a series of mutually exclusive lineage labels (A,B,C...). After the minimum proportion of samples are labeled with mutually exclusive lineages, each of the resulting labels is independently subdivided by the same process (e.g. A is divided into A.1, A.2, A.3... until the minimum proportion of A samples are labeled with an A.X lineage). 
+
+## Run Locally
+
+This app has minimal dependencies, relying only on streamlit and Python's standard libraries.
+
+Install it with:
+
+```
+pip install streamlit
+git clone https://github.com/jmcbroome/automated-lineage-json
+cd automated-lineage-json
+```
+
+And run with:
+
+```
+streamlit run streamlit_app.py
+```
--- streamlit_app.py
+++ streamlit_app.py
@@ -17,17 +17,26 @@ st.set_page_config(layout='wide')
 with st.form(key="autolin"):
     st.markdown("# AUTOLIN")
     st.markdown("This app is a tool that uses the genotype representation score heuristic to add lineage nomenclature labels to a Nextstrain Auspice JSON.")
-    st.markdown("The Nextstrain JSON files produced by this tool can be uploaded to [Auspice](https://auspice.us/) for viewing.")
-    size = st.number_input("Minimum number of samples to define a lineage.",min_value=1)
-    distinction = st.number_input("Minimum number of distinguishing mutations to define a lineage.",min_value=1)
+    st.markdown(" ".join([
+        "The generated nomenclature is genotype-based and hierarchical, with a simplified Pango-style naming schema.",
+        "For example, the lineage A.1.1 is a sublineage of A.1, which in turn is a sublineage of group A.",
+        "Each of the these would be considered a 'level' of annotation.",
+        "The nomenclature is generated iteratively; each 'level' is generated as a series of mutually exclusive lineage labels (A,B,C...).",
+        "After the minimum proportion of samples are labeled with mutually exclusive lineages, each of the resulting labels is independently",
+        "subdivided by the same process (e.g. A is divided into A.1, A.2, A.3... until the minimum proportion of A samples are labeled with an A.X lineage).",
+        "Lineage label generation ceases when no candidate lineage roots fulfill conditions set by the user or the maximum number of levels have been generated."
+    ]))
+    st.markdown("The Nextstrain JSON files produced by this tool can be uploaded to [Auspice](https://auspice.us/) for viewing. For convenience, a view of auspice.us is embedded below.")
+    size = st.number_input("Output lineages will contain at least this many samples.",min_value=1)
+    distinction = st.number_input("Output lineages will have at least this many mutations distinguishing them from their parent lineage or the tree root.",min_value=1)
     cutoff = st.number_input("Proportion of samples that should be covered at each level of lineage annotation.",min_value=0.0,max_value=1.0,value=0.95)
     levels = st.number_input("Maximum number of levels to generate. Set to 0 to generate as many as possible.",min_value=0)
-    floor = st.number_input("Minimum genotype representation score to annotate a lineage.",min_value=0)
-    missense = st.checkbox("Consider amino-acid altering mutations across the genome only.")
-    gene = st.text_input("Limit considered mutations to amino-acid altering mutations in a specific gene. Leave blank to consider mutations in any gene.",value="")
-    uploaded_file = st.file_uploader("Choose a JSON to generate lineage labels from.")
+    floor = st.number_input("Minimum genotype representation score to annotate a lineage. This is an abstract value that considers both the number and distinction of descendent samples. Set to a higher value to prevent the designation of marginal lineage labels representing few or similar samples.",min_value=0)
+    missense = st.checkbox("Consider only amino-acid altering mutations across the genome.")
+    gene = st.text_input("Limit considered mutations to amino-acid altering mutations in a specific gene, named here. Leave blank to consider mutations in any gene. Ensure that the gene is present in your input JSON!",value="")
+    uploaded_file = st.file_uploader("Upload a JSON to generate lineage labels from.")
     runbutton = st.form_submit_button(label='Generate the labeled JSON and table.')
-    st.markdown("Once downloaded, you can drag and drop the results file into the view below, or to a [separate tab.](https://auspice.us/)")
+    st.markdown("Once downloaded, you can drag and drop the annotated JSON into the view below, or to a [separate tab.](https://auspice.us/)")
     st.markdown("You have to download the results file first as Auspice is rendered client-side.")
 
 pref = _get_session()
